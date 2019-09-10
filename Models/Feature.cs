98e21682b756ce3b29042d6ba0ac1eb23346db94
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2019-09-10 17:32:00 +0200

    aggiunte info per fragment xml, allow e deny ISO

--- Models/Feature.cs
+++ Models/Feature.cs
@@ -6,6 +6,9 @@
         public string Functionality { get; set; }
         public string Tag {get; set;}
         public bool Discontinued {get; set;}
+        public string Fragment {get; set;}
+        public string AllowISO {get; set;}
+        public string DenyISO {get; set;}
         public string Standard {get; set;}
         public string Professional {get; set;}
         public string Enterprise {get; set;}
